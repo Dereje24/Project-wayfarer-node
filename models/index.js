ffea40efc08b9b30194f541732f27446039ded1f
Dereje24 <ddilnesaw@yahoo.com>
2018-04-02 16:23:40 -0700

    added mongoURI for heroku

--- models/index.js
+++ models/index.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 var Location = require('./location');
 var Post = require('./post');
 var User = require('./user');
+var dbUrl = process.env.MONGODB_URI || 'mongodb://localhost/wayfarer';
 
-mongoose.connect('mongodb://localhost/wayfarer', {promiseLibrary: global.Promise});
+mongoose.connect(dbUrl, {promiseLibrary: global.Promise});
 
 module.exports = {
   User: User,
